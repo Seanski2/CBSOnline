6de79efa92035b61a585a643decc1ce446c20a54
Minemaker0430 <85040155+Minemaker0430@users.noreply.github.com>
2023-09-09 14:03:41 -0600

    changed some stuff

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import "./character-data.json";
-import "../assets";
-
-//globals
-TEAM_A = {};
-TEAM_B = {};
-CRIT_MULTIPLIER = 1.33333;
---
+++ src/main.vue
@@ -0,0 +1,32 @@
+<script>
+
+import "./character-data.json";
+import "../assets";
+
+//globals
+CRIT_MULTIPLIER = 1.33333;
+
+//variables
+//only use when attacks happen. a is the local player, b is the online player.
+a;
+a.atk;
+a.def;
+a.mat;
+a.mdf;
+a.hp;
+a.spd;
+a.mp;
+b;
+b.atk;
+b.def;
+b.mat;
+b.mdf;
+b.hp;
+b.spd;
+b.mp;
+
+//team setup: each team can have up to 4 people. here we use dummies as a placeholder and replace the values later. once again, a is local, b is online
+teamA = {dummy, dummy, dummy, dummy}
+teamB = {dummy, dummy, dummy, dummy}
+
+</script>
