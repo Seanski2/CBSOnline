2774de79f468481d4d776a18bdfa214ee56ce19b
Minemaker0430 <85040155+Minemaker0430@users.noreply.github.com>
2023-10-02 15:18:15 -0600

    i am suffering

---
+++ src/data/data.gd
@@ -0,0 +1,7 @@
+extends "res://src/data/"
+
+#Add a new path in these variables when you add new data.
+var characters;
+var elements = "res://src/data/elements/";
+var moves = "res://src/data/moves/";
+var statuses = "res://src/data/statuses/";
