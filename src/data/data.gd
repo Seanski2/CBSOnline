b482e37bb7d6f3d24ddd971b59254c3008dc3330
Minemaker0430 <85040155+Minemaker0430@users.noreply.github.com>
2023-10-02 15:31:12 -0600

    data system will be worked on later

--- src/data/data.gd
+++ src/data/data.gd
@@ -1,5 +1,5 @@
 #Add a new path in these variables when you add new data.
 var characters;
-var elements = "res://src/data/elements/";
-var moves = "res://src/data/moves/";
-var statuses = "res://src/data/statuses/";
+var elements;
+var moves;
+var statuses;
