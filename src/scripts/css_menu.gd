b76cd1d9eb2f171ef606bb18c77e0e3540606ab5
Minemaker0430 <85040155+Minemaker0430@users.noreply.github.com>
2023-10-02 18:29:10 -0600

    Selection Visuals

--- src/scripts/css_menu.gd
+++ src/scripts/css_menu.gd
@@ -3,3 +3,4 @@ extends Control
 #Define Globals
 var teamSelected = 0;
 var myTeam = ["null", "null", "null", "null"];
+
--- src/scripts/select_character.gd
+++ src/scripts/select_character.gd
@@ -3,10 +3,23 @@ extends TextureButton
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _pressed():
 	if (CSSMenuVars.teamSelected <= 3) and not (CSSMenuVars.myTeam.has(get_meta("CharacterId"))):
+		
+		var selectionObjects = [
+			$"../../Selection Indications/Selection 1",
+			$"../../Selection Indications/Selection 2", 
+			$"../../Selection Indications/Selection 3", 
+			$"../../Selection Indications/Selection 4"
+		];
+		
+		selectionObjects[CSSMenuVars.teamSelected].set_position(self.get_screen_position());
+		selectionObjects[CSSMenuVars.teamSelected].visible = true;
+
+		
 		CSSMenuVars.myTeam[CSSMenuVars.teamSelected] = get_meta("CharacterId");
 		CSSMenuVars.teamSelected += 1;
 		$"../../Undo".disabled = false;
 		$"../../Reset".disabled = false;
 		$"../../SFX".playing = true;
+		
 	print(CSSMenuVars.teamSelected)
 	print(CSSMenuVars.myTeam)
