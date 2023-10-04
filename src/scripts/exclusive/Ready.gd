36dc0c1e3fa6ba811b87bb3995d46ec876856747
Minemaker0430 <85040155+Minemaker0430@users.noreply.github.com>
2023-10-03 20:44:02 -0600

    changed ui and scripts a bit

--- src/scripts/exclusive/Ready.gd
+++ src/scripts/exclusive/Ready.gd
@@ -1,6 +1,4 @@
 extends TextureButton
 
 func _pressed():
-	CSSMenuVars.teamSelected = 0
-	CSSMenuVars.myTeam = ["null", "null", "null", "null"]
 	get_tree().change_scene_to_file(get_meta("loadScene"))
