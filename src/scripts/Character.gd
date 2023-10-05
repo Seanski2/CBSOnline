5d99f800ebd02b24c7ead2c045b554fa4c74f20c
JHDeucey <deuceydu@gmail.com>
2023-10-04 19:32:51 -0500

    Added variable to Character class for stats and basicAttack function
    Grabs stats from jsonData in the constructor for Character, basicAttack function is called from the attacker and has the recipient as a paramater, with optional parameters for base power and melee/magic, added "name" parameter to Mari's json (needs to be done for every character)

--- src/scripts/Character.gd
+++ src/scripts/Character.gd
@@ -7,13 +7,38 @@ var jsonData = {}
 var internalName = ""
 
 var name = ""
+var displayName
+var element1
+var element2
+
+var maxHealth
+var speed
+var magic
+var attack
+var defense
+var magAttack
+var magDefense
+
 var health
 
 func _init(charName: String):
+	
 	internalName = charName
+	
 	jsonPath = jsonPath + internalName
 	jsonData = load_json(jsonPath)
-	pass
+	
+	name = jsonData["name"]
+	maxHealth = jsonData[name]["baseStats"]["health"]
+	speed = jsonData[name]["baseStats"]["speed"]
+	magic = jsonData[name]["baseStats"]["magic"]
+	attack = jsonData[name]["baseStats"]["attack"]
+	defense = jsonData[name]["baseStats"]["defense"]
+	magAttack = jsonData[name]["baseStats"]["magicAttack"]
+	magDefense = jsonData[name]["baseStats"]["magicDefense"]
+	
+	health = maxHealth
+	
 
 func load_json(filePath: String):
 	if FileAccess.file_exists(filePath):
@@ -27,3 +52,15 @@ func load_json(filePath: String):
 		
 	else:
 		print("Character file not found for " + internalName)
+
+func basicAttack(char: Character, isPhys: bool = true, basePower: int = 4):
+	var attackStat
+	var defenseStat
+	if isPhys:
+		attackStat = attack
+		defenseStat = char.defense
+	else:
+		attackStat = magAttack
+		defenseStat = char.magDefense
+	var damage = (9 * basePower * attackStat)/defenseStat
+	char.health -= damage
