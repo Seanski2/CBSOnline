a136a1c86633afe0c73ae694bc9edd1491720cc2
Minemaker0430 <85040155+Minemaker0430@users.noreply.github.com>
2023-10-10 15:12:09 -0600

    added battlevars

---
+++ src/scripts/BattleVars.gd
@@ -0,0 +1,104 @@
+#Constants
+
+#Globals
+var allies = {
+	"1": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	},
+	"2": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	},
+	"3": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	},
+	"4": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	}
+}
+
+var enemies = {
+	"1": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	},
+	"2": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	},
+	"3": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	},
+	"4": {
+		"char": Character.new("template.json"),
+		"hp": 0,
+		"mp": 0,
+		"maxhpMods": 0,
+		"maxmpMods": 0,
+		"atkMods": 0,
+		"defMods": 0,
+		"matMods": 0,
+		"mdfMods": 0,
+		"spdMods": 0
+	}
+}
--- src/scripts/exclusive/Ready.gd
+++ src/scripts/exclusive/Ready.gd
@@ -1,4 +1,9 @@
 extends TextureButton
 
 func _pressed():
+	BattleVars.allies["1"]["char"] = CSSMenuVars.myTeam[0]
+	BattleVars.allies["2"]["char"] = CSSMenuVars.myTeam[1]
+	BattleVars.allies["3"]["char"] = CSSMenuVars.myTeam[2]
+	BattleVars.allies["4"]["char"] = CSSMenuVars.myTeam[3]
+	
 	get_tree().change_scene_to_file ("res://src/scenes/vs.tscn")
